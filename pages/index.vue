c787ad230abb2cf792de1fb003367ecc44291ba5
OscarSilvaOfficial <OscarSilvaOfficial>
2022-03-20 00:11:46 -0300

    criação do projeto

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-row justify="center" align="center">
+    <video width="320" height="240" controls>
+      <source :src="video" type="video/mp4">
+    </video>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data: () => ({
+    video: process.env.videoAPI
+  })
+}
+</script>
